99a0f5fa3c1a6eddeec5d4a6439c6fbeeed14e8a
keithvenh <keith.venhuizen@gmail.com>
2022-03-22 12:28:19 -1000

    Organize file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './app/assets/stylesheets/index.scss';
+import App from './app/components/App';
+import reportWebVitals from './app/helpers/application/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
