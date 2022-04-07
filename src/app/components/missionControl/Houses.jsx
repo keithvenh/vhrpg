b51b394bd506bc56614cbc609bf4017b5f250ea3
Keith VenHuizen <keith.venhuizen@gmail.com>
2022-04-06 15:22:54 -1000

    Add character movement

---
+++ src/app/components/missionControl/Houses.jsx
@@ -0,0 +1,29 @@
+
+export default function Houses() {
+
+    const mission = require('../../assets/images/mission.png');
+    const hangar = require('../../assets/images/hangar.png');
+    const equipment = require('../../assets/images/equipment.png');
+    const weapons = require('../../assets/images/weapons.png');
+    const characters = require('../../assets/images/characters.png');
+    const health = require('../../assets/images/health.png');
+    const stats = require('../../assets/images/stats.png');
+    const rules = require('../../assets/images/rules.png');
+
+    return (
+
+        <div id='houses-container' className="houses-container">
+
+            <img className='house' src={mission} alt='Missions' />
+            <img className='house' src={hangar} alt='Hangar' />
+            <img className='house' src={equipment} alt='Equipment' />
+            <img className='house' src={weapons} alt='Weapons' />
+            <img className='house' src={characters} alt='Characters' />
+            <img className='house' src={health} alt='Health' />
+            <img className='house' src={stats} alt='Stats' />
+            <img className='house' src={rules} alt='Rules' />
+
+        </div>
+
+    )
+}
--- src/app/components/missionControl/MissionControl.jsx
+++ src/app/components/missionControl/MissionControl.jsx
@@ -1,13 +1,66 @@
 import logout from '../../helpers/auth/logout';
+import Houses from './Houses';
+import { useEffect } from 'react';
 
 function MissionControl(props) {
 
+    let int = Math.floor(Math.random()*2);
+    let char;
+    if(int === 0) {
+      char = "Pax";
+    } else {
+      char = "Ru"
+    }
+    const image = require('../../assets/images/' + char + '-Goes-Walking.gif');
+
+    useEffect(() => {
+        // get the character and the houses
+        const char = document.getElementById('village-character');
+        const houses = document.getElementById('houses-container');
+        let charLoc = 120;
+        let facing = 'right';
+
+        function moveScene(event, character, village) {
+
+            let width = village.offsetWidth;
+            console.log(width);
+            if(event.key === "ArrowRight" && (charLoc < (width - (character.offsetWidth + 32)))) {
+                charLoc += 30;
+                character.style.left = charLoc + 'px';
+                if(facing === 'left') {
+                    char.style.transform = "scaleX(1)";
+                    facing = 'right';
+                }
+            }
+            else if(event.key === "ArrowLeft" && (charLoc > 32)) {
+                charLoc -= 30;
+                character.style.left = charLoc + 'px';
+                if(facing === 'right') {
+                    char.style.transform = "scaleX(-1)";
+                    facing = "left";
+                }
+            }
+            console.log(character);
+            console.log(village);
+
+        }
+  
+        // listen for a keypress event
+        document.addEventListener("keydown", (e) => moveScene(e, char, houses));
+      })
+
     return (
-        <div className='loading'>
-            <h1><span className='sw'>Welcome</span>, <br /> {props.user.email}</h1>
+        <div className='mission-control'>
             <div className='settings-cog'>
                 <p><i className='fas fa-cog'></i></p>
                 <p className='logout-link' onClick={logout}>Logout</p>
+            </div>
+            <div className='village'>
+                <Houses />
+                <div className='sidewalk'>
+
+                </div>
+                <img id='village-character' className='village-character' src={image} alt='' />
             </div>
         </div>
     )
