07ea6c82f5958fdb620830463971c0c05fa9b652
keithvenh <keith.venhuizen@gmail.com>
2022-04-12 22:38:06 -1000

    Design Mission Control with Click Handling

--- src/app/components/missionControl/MissionControl.jsx
+++ src/app/components/missionControl/MissionControl.jsx
@@ -1,72 +1,6 @@
 import logout from '../../helpers/auth/logout';
-import Houses from './Houses';
-import { useEffect } from 'react';
 
 function MissionControl(props) {
-
-    const up = require('../../assets/images/characterUp.png');
-    const down = require('../../assets/images/characterDown.png');
-    const left = require('../../assets/images/characterLeft.png');
-    const right = require('../../assets/images/characterRight.png');
-    let image = down;
-
-    useEffect(() => {
-        // get the character and the houses
-        const char = document.getElementById('village-character');
-        const houses = document.getElementById('houses-container');
-        const mc = document.getElementById('mission-control');
-        let charX = (mc.offsetWidth/2) - (char.offsetWidth/2);
-        let charY = (mc.offsetHeight/2) - (char.offsetHeight/2);
-        char.style.left = charX + 'px';
-        char.style.top = charY + 'px';
-
-        function moveScene(event, character, village, control) {
-
-            let villageWidth = village.offsetWidth;
-            let controlWidth = control.offsetWidth;
-            let padding = control.offsetWidth - village.offsetWidth;
-            let charWidth = character.offsetWidth;
-            let leftWall = (padding/2);
-            let rightWall = ((padding/2) + villageWidth) - (charWidth);
-            let bottomWall = control.offsetHeight - character.offsetHeight;
-            if(event.key === "ArrowRight" && (charX < rightWall)) {
-                charX += 30;
-                if(charX > rightWall) {
-                    charX = rightWall;
-                }
-                character.style.left = charX + 'px';
-                char.src = right;
-            }
-            else if(event.key === "ArrowLeft" && (charX > leftWall)) {
-                charX -= 30;
-                if(charX < leftWall) {
-                    charX = leftWall;
-                }
-                character.style.left = charX + 'px';
-                char.src = left;
-            }
-            else if(event.key === "ArrowUp" && (charY > 0)) {
-                charY -= 30;
-                if(charY < 0) {
-                    charY = 0;
-                }
-                character.style.top = charY + 'px';
-                char.src = up;
-            }
-            else if(event.key === "ArrowDown" && (charY < bottomWall)) {
-                charY += 30;
-                if(charY > bottomWall) {
-                    charY = bottomWall;
-                }
-                character.style.top = charY + 'px';
-                char.src = down;
-            }
-
-        }
-  
-        // listen for a keypress event
-        document.addEventListener("keydown", (e) => moveScene(e, char, houses, mc));
-      })
 
     return (
         <div id='mission-control' className='mission-control'>
@@ -74,23 +8,74 @@ function MissionControl(props) {
                 <p><i className='fas fa-cog'></i></p>
                 <p className='logout-link' onClick={logout}>Logout</p>
             </div>
-            <div className='village'>
-                <div className='row'>
 
-                    <div className='rules' id='rules'></div>
-                    <div className='street vert four'></div>
-                    <div className='park' id='park'></div>
-                    <div className='wall vert four'></div>
-                    <div className='businessScene' id='businessScene'></div>
-                    <div className='darkShroud' id='darkShroud'></div>
-                    <div className='sithTemple' id='sithTemple'></div>
-
+            <div className='screen-container'>
+                <div className='screen orange characterManagement' id='characterManagement' onClick={() => props.updateView('characterManagement')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Character Management</h2>
+                        <p className='sw'>Character Management</p>
+                    </div>
                 </div>
-                <Houses />
-                <div className='sidewalk'>
 
+                <div className='screen blue square gear' id='gear' onClick={() => props.updateView('gear')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Gear</h2>
+                        <p className='sw'>Gear</p>
+                    </div>
                 </div>
-                <img id='village-character' className='village-character' src={image} alt='' />
+
+                <div className='screen teal square talentTrees' id='talentTrees' onClick={() => props.updateView('talentTrees')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Talent Trees</h2>
+                        <p className='sw'>Talent Trees</p>
+                    </div>
+                </div>
+
+                <div className='screen red square vehicles' id='vehicles' onClick={() => props.updateView('vehicles')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Vehicles</h2>
+                        <p className='sw'>Vehicles</p>
+                    </div>
+                </div>
+
+                <div className='screen green currentMission' id='currentMission' onClick={() => props.updateView('currentMission')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Current Mission</h2>
+                        <p className='sw'>Current Mission</p>
+                    </div>
+                    <div className='missionDetails'>
+                        <p className='currentLocation'>Location: Koler</p>
+                    </div>
+                </div>
+
+                <div className='screen blue square library' id='library' onClick={() => props.updateView('library')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Library</h2>
+                        <p className='sw'>Library</p>
+                    </div>
+                </div>
+
+                <div className='screen teal square forcePowers' id='forcePowers' onClick={() => props.updateView('forcePowers')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Force Powers</h2>
+                        <p className='sw'>Force Powers</p>
+                    </div>
+                </div>
+
+                <div className='screen red square casino' id='casino' onClick={() => props.updateView('casino')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Casino</h2>
+                        <p className='sw'>Casino</p>
+                    </div>
+                </div>
+
+                <div className='screen yellow organizationManagement' id='currentMission' onClick={() => props.updateView('currentMission')}>
+                    <div className='screenTitle'>
+                        <h2 className='title'>Organization Management</h2>
+                        <p className='sw'>Organization Management</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
