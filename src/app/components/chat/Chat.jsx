d2d9af14c2319c2f9c81cb17ec2dd88cfa486669
keithvenh <keith.venhuizen@gmail.com>
2022-04-15 15:27:53 -1000

    Add ability to send messags

--- src/app/components/chat/Chat.jsx
+++ src/app/components/chat/Chat.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { getDocs, collection, doc, onSnapshot, query, orderBy, limit, setDocs } from 'firebase/firestore';
-import { db } from '../../../config/firebase';
+import { getDocs, collection, doc, onSnapshot, query, orderBy, limit, addDoc, serverTimestamp } from 'firebase/firestore';
+import { db, auth } from '../../../config/firebase';
 import Message from './Message';
 
 class Chat extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        messages: []
+        messages: [],
+        messageInput: '',
+        expanded: true
       }
       this.toggleChat = this.toggleChat.bind(this);
       this.getMessages = this.getMessages.bind(this);
+      this.validateMessage = this.validateMessage.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async getMessages(db) {
@@ -38,8 +42,34 @@ class Chat extends React.Component {
 
     }
 
+    validateMessage(e) {
+      this.setState({messageInput: e.target.value})
+    }
+
+    async submitMessage(message) {
+      console.log(auth.currentUser.uid);
+      const newMessage = await addDoc(collection(db, "channels/chat/messages"), {
+        userId: auth.currentUser.uid,
+        text: message.trim(),
+        createdAt: serverTimestamp()
+      });
+      return newMessage;
+    }
+
+    handleSubmit(e) {
+      if(e.key == 'Enter' && this.state.messageInput != '') {
+        this.submitMessage(this.state.messageInput);
+        this.setState({messageInput: ''})
+      }
+    }
+
     componentDidMount() {
       this.getMessages(db);
+      document.getElementById('messagesEnd').scrollIntoView({behavior: 'smooth'});
+    }
+
+    componentDidUpdate() {
+      document.getElementById('messagesEnd').scrollIntoView({behavior: 'smooth'});
     }
 
     render() {
@@ -47,10 +77,19 @@ class Chat extends React.Component {
       return (
         <div className={`userChat ${this.state.expanded ? 'expanded' : 'collapsed'} ${this.props.currentView == 'loading' ? 'hidden' : ''}`} id='userChat'>
             <i className='toggle' onClick={() => this.toggleChat()}>{this.state.expanded ? String.fromCharCode(9660) : String.fromCharCode(9650)}</i>
-            <div className='messages-container'>
-              {this.state.messages.map(message => (<Message key={message.id} data={message} />))}
+            <div className='messages-container' id='messages-container'>
+              <div className='messages' id='messages'>
+                {this.state.messages.map(message => (<Message key={message.id} data={message} />))}
+                <div className='messagesEnd' id='messagesEnd'></div>
+              </div>
             </div>
-            <input className='userChatInput' placeholder='Message' autoFocus />
+            <input 
+              className='userChatInput' 
+              placeholder='Message' 
+              value={this.state.messageInput} 
+              onChange={(e) => this.validateMessage(e)}
+              onKeyPress={(e) => this.handleSubmit(e)} 
+              autoFocus />
         </div>
       );
   
