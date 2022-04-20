7944e94b793bd8099d0625e5c38ab91f8d628c5d
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2022-04-19 22:38:17 -0500

    create character hud container

---
+++ src/app/components/currentMission/CharacterHUD.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CharacterHUD() {
+  
+
+    return (
+      <div className='screen-container'>
+          <div className='primary-hud orange' id='primary-hud'>
+              <div className='hudTitle'>
+                  <h2 className='title'>Primary Character</h2>
+                  <p className='sw'>Primary Character</p>
+              </div>
+          </div>
+      </div>
+    )
+}
