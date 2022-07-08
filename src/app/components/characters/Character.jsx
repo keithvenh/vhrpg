90774b67ec1f1a788911047c49889c740d4269f1
keithvenh <keith.venhuizen@gmail.com>
2022-07-07 21:51:16 -1000

    Add structure to character page

--- src/app/components/characters/Character.jsx
+++ src/app/components/characters/Character.jsx
@@ -1,6 +1,36 @@
+// MAY NEED TO CHANGE THIS TO A CLASS BASED COMPONENT. 
+// WE WILL DEFINITELY NEED HELPER FUNCTIONS TO LOOP THROUGH AND DISPLAY DIFFERENT ASPECTS LIKE SKILLS AND TALENTS
+// I NEED TO RESEARCH HOW TO RETURN CHUNKS OF JSX WITH A STANDARD FUNCTION AND PLUG IT INTO OTHER SECTIONS
+
 function Character(props) {
     return(
-        <p>CHARACTER PAGE FOR: {props.character.displayName}</p>
+        <div className='character-container'>
+            <div className='character-image'>
+                {/* TRYING TO DECIDE WHAT TYPE AND HOW TO DISPLAY A CHARACTER IMAGE */}
+            </div>
+            <div className='character-details'>
+                <div className='character-overview'>
+
+                    <h1 className='character-name'>{props.character.displayName}</h1>
+
+                </div>
+                <div className='character-characteristics'>
+                    <p className='brawn'>Brawn: {props.character.characteristics.brawn}</p>
+                    <p className='brawn'>Agility: {props.character.characteristics.agility}</p>
+                    <p className='brawn'>Intellect: {props.character.characteristics.intellect}</p>
+                    <p className='brawn'>Cunning: {props.character.characteristics.cunning}</p>
+                    <p className='brawn'>Willpower: {props.character.characteristics.willpower}</p>
+                    <p className='brawn'>Presence: {props.character.characteristics.presence}</p>
+                </div>
+                <div className='character-skills'>
+                    {/* WE NEED TO FIGURE OUT HOW TO LOOP THROUGH props.character.skills */}
+
+                </div>
+            </div>
+            <div className='character-talents'>
+                {/* WE NEED TO FIGURE OUT HOW TO LOOP THROUGH props.character.talents */}
+            </div>
+        </div>
     )
 }
 
