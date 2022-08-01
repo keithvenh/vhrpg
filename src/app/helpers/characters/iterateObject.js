121e144d76f05882cd0eca5c522db5a8d467f22d
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2022-07-31 21:08:36 -0500

    alphabetized arrays

--- src/app/helpers/characters/iterateObject.js
+++ src/app/helpers/characters/iterateObject.js
@@ -5,6 +5,20 @@ export default function iterateObject(obj) {
         
         myArray.push(obj[key])
     })
-    
+    console.log(myArray);
+    //Sort alphabetically
+    myArray.sort((a,b) => {
+        let fa = a.name.toLowerCase(),
+            fb = b.name.toLowerCase();
+
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(myArray);
     return myArray
 }
